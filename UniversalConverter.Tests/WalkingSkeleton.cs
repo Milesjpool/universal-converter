168b2d6dd141562895c0b0f1d68fb9a5063d6c93
Miles Pool <MilesJPool@gmail.com>
2014-10-22 14:56:31 +0100

    Created solution with test, spec and web projects

---
+++ UniversalConverter.Tests/WalkingSkeleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+
+namespace UniversalConverter.Tests
+{
+	[TestFixture]
+    public class WalkingSkeleton
+    {
+		[Test]
+		public void Should_get_an_okay_response_from_the_server()
+		{
+			var uri = new Uri("Http://universal-converter.local/");
+			var webRequest = WebRequest.Create(uri);
+			var webResponse = (HttpWebResponse) webRequest.GetResponse();
+			Assert.That(webResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+		}
+
+	}
+}
