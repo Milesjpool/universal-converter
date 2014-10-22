8e0a43be5bbaa7889d6f4f51e7fd55fa3bf18e57
Miles Pool <Miles.pool@7digital.com>
2014-10-22 16:00:26 +0100

    Got working skeleton working.

---
+++ UniversalConverter.Web/HttpHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Web;
+
+namespace UniversalConverter.Web
+{
+    public class HttpHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.StatusCode = (int) HttpStatusCode.OK;
+        }
+
+        public bool IsReusable { get; private set; }
+    }
+}
